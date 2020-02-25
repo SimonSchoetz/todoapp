0a839f0bc0426ee51965148d706083fc33a66611
Simon <simonsch.tz@gmail.com>
2020-02-25 13:26:28 +0100

    Set up project

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import "../css/App.scss"
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My React App</h1>
+    </div>
+  );
+}
+
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+
+import React from "react"
+import App from "./components/App"
+import reactDOM from "react-dom"
+import "./css/index.css"
+
+reactDOM.render(<App />, document.getElementById("root")) 
