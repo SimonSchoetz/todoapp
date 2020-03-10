7f177f37791fcb40455ab94c8844693ca7223bb5
Simon <simonsch.tz@gmail.com>
2020-03-10 10:18:29 +0100

    Updates

---
+++ precache-manifest.fab7c8a3ffbe5d2236ac4d454675fee9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83a3beed8e12fd3bd3e414037ce2d391",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b94d57e4da8749ad4c7d",
+    "url": "./static/css/main.74e0c90a.chunk.css"
+  },
+  {
+    "revision": "7bb3b3118b0f650fa947",
+    "url": "./static/js/2.9b51bb38.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "./static/js/2.9b51bb38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b94d57e4da8749ad4c7d",
+    "url": "./static/js/main.7347f36b.chunk.js"
+  },
+  {
+    "revision": "7d2d16499b417a029b08",
+    "url": "./static/js/runtime-main.dc7f9edc.js"
+  }
+]);
